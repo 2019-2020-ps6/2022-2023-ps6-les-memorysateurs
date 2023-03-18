afdda58a574c77ef5316950166e06d3b00889611
Dorian-Bouchard <dorian.bouchard@etu.unice.fr>
2023-03-18 22:42:18 +0100

    feat: ajout ThemeService Ref #8

---
+++ front-end/src/app/services/theme.service.ts
@@ -0,0 +1,13 @@
+import {Injectable} from "@angular/core";
+import {BehaviorSubject} from "rxjs";
+import {Theme} from "../../models/theme.models";
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class ThemeService {
+  public listeThemes$: BehaviorSubject<Theme[]> = new BehaviorSubject<Theme[]>([]);
+
+  
+}
