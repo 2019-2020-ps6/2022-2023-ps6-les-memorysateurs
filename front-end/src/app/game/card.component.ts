97112bc3c63cf72e156cf01977abb5030a4c40da
Benjamin Neuville <benjamin.neuville-burguiere@etu.unice.fr>
2023-03-20 08:33:37 +0100

    board game

--- front-end/src/app/game/card.component.ts
+++ front-end/src/app/game/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, Inject } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 
 @Component({
@@ -9,12 +9,21 @@ import { Component, OnInit, Input, Inject } from '@angular/core';
 })
 
 export class Card implements OnInit {
+onImageClick(event: Event) {
+  event.stopPropagation();
+  this.parentClick.emit(event);
+}
   ngOnInit(): void {}
   constructor() {
   }
 
+  OnClick(event: any) : void {};
   @Input() public picture: string = "../../assets/images/1.png";
   @Input() public numCard: number = 0;
   @Input() public name: string = "test";
   @Input() public isFlipped: boolean = false;
+
+  @Output() parentClick : EventEmitter<Event> = new EventEmitter();
+
+
 }
--- front-end/src/app/game/cardcontainer.component.ts
+++ front-end/src/app/game/cardcontainer.component.ts
@@ -1,5 +1,16 @@
 import { Card } from './card.component';
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit, Directive, HostListener, ViewChildren, QueryList, AfterViewInit } from '@angular/core';
+
+// @Directive({selector: 'button[counting]'})
+
+// class CountClicks {
+//   numberOfClicks = 0;
+
+//   @HostListener('click', ['$event.target'])
+//   onClick(btn) {
+//     console.log('button', btn, 'number of clicks:', this.numberOfClicks++);
+//   }
+// }
 
 @Component({
   selector: 'app-cardcontainer',
@@ -7,21 +18,79 @@ import { Component, OnInit} from '@angular/core';
   styleUrls: ['./cardcontainer.component.scss']
 })
 
-export class CardsContainer implements OnInit {
-  ngOnInit(): void {}
-  constructor() {}
+export class CardsContainer implements OnInit, AfterViewInit {
   cards = [
     {picture: "../../assets/images/1.png", numCard: 1, name: "1", isFlipped: false},
     {picture: "../../assets/images/1.png", numCard: 2, name: "2", isFlipped: false},
+    {picture: "../../assets/images/1.png", numCard: 1, name: "1", isFlipped: false},
     {picture: "../../assets/images/1.png", numCard: 3, name: "3", isFlipped: false},
+    {picture: "../../assets/images/1.png", numCard: 2, name: "2", isFlipped: false},
     {picture: "../../assets/images/1.png", numCard: 4, name: "4", isFlipped: false},
-    {picture: "../../assets/images/1.png", numCard: 5, name: "5", isFlipped: false},
-    {picture: "../../assets/images/1.png", numCard: 6, name: "6", isFlipped: false},
-    {picture: "../../assets/images/1.png", numCard: 7, name: "7", isFlipped: false},
-    {picture: "../../assets/images/1.png", numCard: 8, name: "8", isFlipped: false},
+    {picture: "../../assets/images/1.png", numCard: 4, name: "4", isFlipped: false},
+    {picture: "../../assets/images/1.png", numCard: 3, name: "3", isFlipped: false},
   ]
+
+  @ViewChildren(Card) children: QueryList<Card> = new QueryList<Card>();
+
+  constructor() {
+  }
+  ngOnInit(): void {
+  }
+  ngAfterViewInit(): void {
+    // this.children.forEach((child) => {
+    //   child.OnClick = this.OnClick;
+    // });
+  }
+
+  public delay(ms: number) {
+    return new Promise( resolve => setTimeout(resolve, ms) );
+}
+  
 
   public getColumns(): string {
     return 'repeat(' + this.cards.length/2 + ', 1fr)';
+  }
+  
+  public async OnClick(event: any) {
+    event.stopPropagation();
+    if(!event?.target.classList.contains('disabled')) {
+      event?.target.classList.toggle('flipped');
+      var flipped = document.getElementsByClassName("flipped");
+      if(flipped.length == 2) {
+        console.log(flipped.length);
+          //TODO : check if the cards are the same
+          if(flipped[1].getAttribute("ng-reflect-name") == flipped[0].getAttribute("ng-reflect-name")) {
+            console.log(flipped[1].getAttribute("ng-reflect-name") + " " + flipped[0].getAttribute("ng-reflect-name"));
+            await this.delay(1000);
+            Array.prototype.forEach.call(flipped, function(card) {
+              card.classList.add('checked');
+            });
+            await this.delay(2000);
+            flipped[0].classList.add('disabled');
+            flipped[1].classList.add('disabled');
+            flipped[0].classList.remove('flipped');
+            event?.target.classList.remove('flipped');
+            
+          }
+          else {
+            //TODO: sleep 1 second before removing the flipped class
+            await this.delay(1000);
+            //TODO
+            flipped[0].classList.add('not-checked');
+            flipped[1].classList.add('not-checked');
+            await this.delay(2000);
+            flipped[0].classList.remove('not-checked');
+            flipped[1].classList.remove('not-checked');
+            event?.target.classList.remove('flipped');
+        }
+      }
+      else if(flipped.length > 2) {
+        Array.prototype.forEach.call(flipped, function(card) {
+          card.classList.remove('flipped');
+        });
+      }
+      var cardsFind = document.getElementsByClassName("card");
+  
+    }
   }
 }
