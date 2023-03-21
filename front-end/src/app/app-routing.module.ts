84639307892ba25a30aae1f53535e773df7b4f6b
RomainPellegrini <romain.pellegrini@etu.unice.fr>
2023-03-21 22:43:04 +0100

    feat : début de l'interface créer un patient Ref #13

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { CreerThemeComponent } from './creer-theme/creer-theme.component';
 import { CreerMemoryComponent } from './creer-memory/creer-memory.component';
 import { FooterCreerThemeComponent } from './footer-creer-theme/footer-creer-theme.component';
 import {ListeThemeComponent} from "./liste-theme/liste-theme.component";
+import {CreerPatientComponent} from "./creer-patient/creer-patient.component";
 
 const routes: Routes = [ {path: 'creer-theme', component: CreerThemeComponent},
   {path: 'creer-memory', component: CreerMemoryComponent},
   {path: 'liste-theme', component: ListeThemeComponent},
-  {path: 'footer-creer-theme', component: FooterCreerThemeComponent}
+  {path: 'footer-creer-theme', component: FooterCreerThemeComponent},
+  {path: 'creer-patient', component: CreerPatientComponent}
   ];
 
 @NgModule({
