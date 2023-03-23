3eab821acf47591688b1a05c4922c67a4dfed75b
Dorian-Bouchard <dorian.bouchard@etu.unice.fr>
2023-03-23 20:13:48 +0100

    feat: ajout PatientService et Moks liste-patient Ref #19

---
+++ front-end/src/moks/liste-patient.moks.ts
@@ -0,0 +1,8 @@
+import {Patient} from "../models/patient.models";
+
+export const LISTE_PATIENT: Patient[] = [
+  new Patient('Perroti', 'Marie', 'assets/images/patient-femme', 3),
+  new Patient('Rombolier', 'Bertrand', 'assets/images/patient-homme', 4),
+  new Patient('Dubois', 'Sophie', 'assets/images/patient-femme', 5),
+  new Patient('Gato', 'Martin', 'assets/images/patient-homme', 4)
+]
