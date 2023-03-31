cfe8d93b536067cf92ebbc0c825f8d94dd69b89b
RomainPellegrini <romain.pellegrini@etu.unice.fr>
2023-03-31 14:17:17 +0200

    feat : quand un bouton est cliqué, les autres sont cachés ref #43

--- front-end/src/app/stat/stat.component.ts
+++ front-end/src/app/stat/stat.component.ts
@@ -14,39 +14,84 @@ export class StatComponent implements OnInit {
   public prenom: string = "";
   public stockImage: string = "";
   public stade: string = "";
-
+  private boutonsTableau : HTMLCollectionOf<Element> |undefined;
   isTempsOpen = false;
   isEssaisOpen = false;
   isErreursOpen = false;
   isIndicesOpen = false;
 
-  constructor(private patientService: PatientService) { }
-  
+  constructor(private patientService: PatientService) {
+     }
+
 
   ngOnInit(): void {
     this.nom = this.patient.value?.nom as string;
     this.prenom = this.patient.value?.prenom as string;
     this.stockImage = this.patient.value?.photo as string;
     this.stade = "Stade " + this.patient.value?.stade as string;
+    this.boutonsTableau= document.getElementsByClassName("bouton-tableau");
 
     // On remplit des données pour l'exemple
     this.partiesJouees = Math.floor(Math.random() * 30);
   }
 
   toggleTemps() {
+
     this.isTempsOpen = !this.isTempsOpen;
+    if(this.boutonsTableau !=undefined)
+      for(let i=0 ;i< this.boutonsTableau.length;i++) {
+        let bouton = this.boutonsTableau.item(i) as HTMLButtonElement;
+        if (bouton.id === "plus-temps") {
+          if(this.isTempsOpen)
+            bouton.innerHTML = "-";
+          else
+            bouton.innerHTML = "+";
+        }
+      }
+
   }
-  
+
   toggleEssais() {
     this.isEssaisOpen = !this.isEssaisOpen;
+    if(this.boutonsTableau !=undefined)
+    for(let i=0 ;i< this.boutonsTableau.length;i++) {
+      let bouton = this.boutonsTableau.item(i) as HTMLButtonElement;
+      if (bouton.id === "plus-essais") {
+        if(this.isEssaisOpen)
+        bouton.innerHTML = "-";
+        else
+          bouton.innerHTML = "+";
+      }
+    }
   }
 
   toggleErreurs() {
     this.isErreursOpen = !this.isErreursOpen;
+    if(this.boutonsTableau !=undefined)
+      for(let i=0 ;i< this.boutonsTableau.length;i++) {
+        let bouton = this.boutonsTableau.item(i) as HTMLButtonElement;
+        if (bouton.id === "plus-erreurs") {
+          if(this.isErreursOpen)
+            bouton.innerHTML = "-";
+          else
+            bouton.innerHTML = "+";
+        }
+      }
+
   }
 
   toggleIndices() {
     this.isIndicesOpen = !this.isIndicesOpen;
+    if(this.boutonsTableau !=undefined)
+      for(let i=0 ;i< this.boutonsTableau.length;i++) {
+        let bouton = this.boutonsTableau.item(i) as HTMLButtonElement;
+        if (bouton.id === "plus-indices") {
+          if(this.isIndicesOpen)
+            bouton.innerHTML = "-";
+          else
+            bouton.innerHTML = "+";
+        }
+      }
   }
 
 }
