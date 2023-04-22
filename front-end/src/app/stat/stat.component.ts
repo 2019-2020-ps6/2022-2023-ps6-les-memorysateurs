8e8e83136edc6d707d0d37e6c41f9a46f126a4c8
RomainPellegrini <romain.pellegrini@etu.unice.fr>
2023-04-22 14:42:01 +0200

    feat: tous les tableaux sont dynamiques ref #56

--- front-end/src/app/stat/stat.component.ts
+++ front-end/src/app/stat/stat.component.ts
@@ -20,6 +20,11 @@ export class StatComponent implements OnInit {
   isErreursOpen = false;
   isIndicesOpen = false;
 
+  progresTemps = false;
+  progresEssais = false;
+  progresErreurs = false;
+  progresIndices = false;
+
   constructor(private patientService: PatientService) {
      }
 
@@ -33,39 +38,10 @@ export class StatComponent implements OnInit {
 
     // On remplit des données pour l'exemple
     this.partiesJouees = <number>this.patient.getValue()?.getStats()?.length;
-    console.log(this.patient.getValue()?.getStat(0));
-
-    let grid = document.getElementById("tab-temps") as HTMLDivElement;
-    let totalTemps = 0;
-    //pour le temps
-    // @ts-ignore
-
-    for( let i =this.patient.getValue()?.getStats()?.length-1; i>=0;i--){
-      let stats = this.patient.getValue()?.getStat(0);
-      let stade = document.createElement('p');
-      stade.innerHTML = "" + this.patient.getValue()?.stade;
-      grid.append(stade);
-      let date = document.createElement('p');
-      let tmp : Date | undefined= this.patient.getValue()?.getStat(i)?.getDate();
-      date.innerHTML = "" +tmp?.getDay() +"/"+ tmp?.getMonth() + "/" +tmp?.getFullYear();
-      grid.append(date);
-      let cartes = document.createElement('p');
-      cartes.innerHTML = "" +this.patient.getValue()?.getStat(i)?.getNbCartes();
-      grid.append(cartes);
-      let temps = document.createElement('p');
-      let secondes = this.patient.getValue()?.getStat(i)?.getTemps();
-      if(secondes != undefined)
-      totalTemps += secondes;
-      // @ts-ignore
-      temps.innerHTML = "" + Math.floor(secondes/60) +"min" + Math.floor(secondes%60) +"s" ;
-      grid.append(temps);
-
-    }
-    let moyenneTemps = document.getElementById("moyenne-temps") as HTMLParagraphElement;
-
-    // @ts-ignore
-    totalTemps = totalTemps/this.patient.getValue()?.getStats()?.length;
-    moyenneTemps.innerHTML = "" + Math.floor(totalTemps/60) +"min" + Math.floor(totalTemps%60) +"s";
+    this.setUpTabTemps();
+    this.setUpTabEssais();
+    this.setUpTabErreurs();
+    this.setUpTabIndices();
   }
 
   toggleTemps() {
@@ -127,4 +103,139 @@ export class StatComponent implements OnInit {
       }
   }
 
+
+  setUpTabTemps(){
+    let grid = document.getElementById("tab-temps") as HTMLDivElement;
+    let totalTemps = 0;
+
+    // @ts-ignore
+
+    for( let i =this.patient.getValue()?.getStats()?.length-1; i>=0;i--){
+      let stats = this.patient.getValue()?.getStat(0);
+      let stade = document.createElement('p');
+      stade.innerHTML = "" + this.patient.getValue()?.stade;
+      grid.append(stade);
+      let date = document.createElement('p');
+      let tmp : Date | undefined= this.patient.getValue()?.getStat(i)?.getDate();
+      date.innerHTML = "" +tmp?.getDay() +"/"+ tmp?.getMonth() + "/" +tmp?.getFullYear();
+      grid.append(date);
+      let cartes = document.createElement('p');
+      cartes.innerHTML = "" +this.patient.getValue()?.getStat(i)?.getNbCartes();
+      grid.append(cartes);
+      let temps = document.createElement('p');
+      let secondes = this.patient.getValue()?.getStat(i)?.getTemps();
+      if(secondes != undefined)
+        totalTemps += secondes;
+      // @ts-ignore
+      temps.innerHTML = "" + Math.floor(secondes/60) +"min" + Math.floor(secondes%60) +"s" ;
+      grid.append(temps);
+
+    }
+    let moyenneTemps = document.getElementById("moyenne-temps") as HTMLParagraphElement;
+
+    // @ts-ignore
+    totalTemps = totalTemps/this.patient.getValue()?.getStats()?.length;
+    moyenneTemps.innerHTML = "" + Math.floor(totalTemps/60) +"min" + Math.floor(totalTemps%60) +"s";
+  }
+
+  setUpTabEssais(){
+    let grid = document.getElementById("tab-essais") as HTMLDivElement;
+    let totalEssais = 0;
+
+    // @ts-ignore
+
+    for( let i =this.patient.getValue()?.getStats()?.length-1; i>=0;i--){
+      let stats = this.patient.getValue()?.getStat(0);
+      let stade = document.createElement('p');
+      stade.innerHTML = "" + this.patient.getValue()?.stade;
+      grid.append(stade);
+      let date = document.createElement('p');
+      let tmp : Date | undefined= this.patient.getValue()?.getStat(i)?.getDate();
+      date.innerHTML = "" +tmp?.getDay() +"/"+ tmp?.getMonth() + "/" +tmp?.getFullYear();
+      grid.append(date);
+      let cartes = document.createElement('p');
+      cartes.innerHTML = "" +this.patient.getValue()?.getStat(i)?.getNbCartes();
+      grid.append(cartes);
+      let essais = document.createElement('p');
+
+
+      // @ts-ignore
+      totalEssais += this.patient.getValue()?.getStat(i)?.getEssais();
+      essais.innerHTML = ""  +this.patient.getValue()?.getStat(i)?.getEssais();
+      grid.append(essais);
+
+    }
+    let moyenneEssais = document.getElementById("moyenne-essais") as HTMLParagraphElement;
+
+    // @ts-ignore
+    totalEssais = totalEssais/this.patient.getValue()?.getStats()?.length;
+    moyenneEssais.innerHTML = ""+ Math.floor(totalEssais);
+  }
+  setUpTabErreurs(){
+    let grid = document.getElementById("tab-erreurs") as HTMLDivElement;
+    let totalErreurs = 0;
+
+    // @ts-ignore
+
+    for( let i =this.patient.getValue()?.getStats()?.length-1; i>=0;i--){
+      let stats = this.patient.getValue()?.getStat(0);
+      let stade = document.createElement('p');
+      stade.innerHTML = "" + this.patient.getValue()?.stade;
+      grid.append(stade);
+      let date = document.createElement('p');
+      let tmp : Date | undefined= this.patient.getValue()?.getStat(i)?.getDate();
+      date.innerHTML = "" +tmp?.getDay() +"/"+ tmp?.getMonth() + "/" +tmp?.getFullYear();
+      grid.append(date);
+      let cartes = document.createElement('p');
+      cartes.innerHTML = "" +this.patient.getValue()?.getStat(i)?.getNbCartes();
+      grid.append(cartes);
+      let erreurs = document.createElement('p');
+
+
+      // @ts-ignore
+      totalErreurs += this.patient.getValue()?.getStat(i)?.getErreurs();
+      erreurs.innerHTML = ""  +this.patient.getValue()?.getStat(i)?.getErreurs();
+      grid.append(erreurs);
+
+    }
+    let moyenneErreurs = document.getElementById("moyenne-erreurs") as HTMLParagraphElement;
+
+    // @ts-ignore
+    totalErreurs = totalErreurs/this.patient.getValue()?.getStats()?.length;
+    moyenneErreurs.innerHTML = ""+ Math.floor(totalErreurs);
+  }
+
+  setUpTabIndices(){
+    let grid = document.getElementById("tab-indices") as HTMLDivElement;
+    let totalIndices = 0;
+
+    // @ts-ignore
+
+    for( let i =this.patient.getValue()?.getStats()?.length-1; i>=0;i--){
+      let stats = this.patient.getValue()?.getStat(0);
+      let stade = document.createElement('p');
+      stade.innerHTML = "" + this.patient.getValue()?.stade;
+      grid.append(stade);
+      let date = document.createElement('p');
+      let tmp : Date | undefined= this.patient.getValue()?.getStat(i)?.getDate();
+      date.innerHTML = "" +tmp?.getDay() +"/"+ tmp?.getMonth() + "/" +tmp?.getFullYear();
+      grid.append(date);
+      let cartes = document.createElement('p');
+      cartes.innerHTML = "" +this.patient.getValue()?.getStat(i)?.getNbCartes();
+      grid.append(cartes);
+      let indices = document.createElement('p');
+
+
+      // @ts-ignore
+      totalIndices += this.patient.getValue()?.getStat(i)?.getIndices();
+      indices.innerHTML = ""  +this.patient.getValue()?.getStat(i)?.getIndices();
+      grid.append(indices);
+
+    }
+    let moyenneIndices = document.getElementById("moyenne-indices") as HTMLParagraphElement;
+
+    // @ts-ignore
+    totalIndices = totalIndices/this.patient.getValue()?.getStats()?.length;
+    moyenneIndices.innerHTML = ""+ Math.floor(totalIndices);
+  }
 }
