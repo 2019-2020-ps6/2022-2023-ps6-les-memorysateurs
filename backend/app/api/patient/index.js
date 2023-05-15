701b8e4da12c98abd1ed25745df9f7802c5116f3
Lucie Andres <lucie.andres@etu.unice.fr>
2023-05-15 11:04:23 +0200

    initialisation ergo #77

--- backend/app/api/patient/index.js
+++ backend/app/api/patient/index.js
@@ -1,6 +1,7 @@
 const { Router } = require('express')
 
 const { Patient } = require('../../models')
+const manageAllErrors = require('../../utils/routes/error-management')
 
 const router = new Router()
 
