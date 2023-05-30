15f34c935de8b7681c977af075c547fc3d0cc054
Lucie Andres <lucie.andres@etu.unice.fr>
2023-05-30 08:47:17 +0200

    #78 annulation test avec dorian

--- backend/app/api/patient/manager.js
+++ backend/app/api/patient/manager.js
@@ -11,9 +11,10 @@ const NotFoundError = require('../../utils/errors/not-found-error.js')
  * This function filters among the patients to return only the patient linked with the given ergoId.
  * @param ergoId
  */
-const filtrerPatientsFromErgo = (idErgo) => {
-  const themes = Theme.get()
-  return themes.filter((theme) => theme.idErgo === idErgo)
+const filterPatientsFromErgo = (ergoId) => {
+  const patients = Patient.get()
+  const parsedId = parseInt(ergoId, 10)
+  return patients.filter((patient) => patient.ergoId === parsedId)
 }
 
 /**
