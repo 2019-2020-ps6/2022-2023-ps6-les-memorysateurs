5a50f847d1c9ccfb1bdd2bf52cd6b4527588b0e9
Lucie Andres <lucie.andres@etu.unice.fr>
2023-06-12 10:50:39 +0200

    reglage probleme de tests bakc et front #112 #111

--- front-end/playwright.config.ts
+++ front-end/playwright.config.ts
@@ -1,13 +1,19 @@
 import { PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
+  testDir: './e2e',
+  reporter: [['html', { open: 'always' }]],
   use: {
-    headless: true,
+    headless: false,
     viewport: { width: 1280, height: 720 },
     ignoreHTTPSErrors: true,
     video: 'on-first-retry',
     screenshot: 'only-on-failure',
+    launchOptions: {
+      slowMo: 1000,
+    }
   },
 };
 
 export default config;
+
